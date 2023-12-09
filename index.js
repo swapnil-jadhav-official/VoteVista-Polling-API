66a78ca8d95dfa7e86b631f7c1b29b72240873e4
Swapnil Jadhav <109851868+swapnil-jadhav-official@users.noreply.github.com>
2023-12-09 21:34:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
+
+//db-connection
+connect();
+
+//routes
 app.use('/questions', questionRoutes);
 
-connect();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
